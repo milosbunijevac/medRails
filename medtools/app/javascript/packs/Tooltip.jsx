b533df5c86304a0fff915f25c65403b5ca27b107
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-08-26 21:52:02 -0400

    "working on box control implementation"

--- medtools/app/javascript/packs/Tooltip.jsx
+++ medtools/app/javascript/packs/Tooltip.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox.jsx';
 
 
 class Tooltip extends React.Component {
   constructor(props){
     super(props);
+    this.commentBoxControl = this.commentBoxControl.bind(this);
   }
 
   render () {
     return (
       <div className="container">
-
         <button onClick={this.props.highL} className="buttons">Highlight</button>
-        <button onClick={this.props.commentT} className="buttons">Comment</button>
+        <button onClick={this.commentBoxControl} className="buttons">Comment</button>
       </div>
     )
+  }
+
+  commentBoxControl() {
+    var div = document.createElement('div');
+    div.className = 'commentBox';
+      ReactDOM.render(
+        <CommentBox />,
+        document.body.appendChild(div),
+      )
+  }
+
+  removeCommentBoxControl() {
+    var elements = document.getElementsByClassName('commentBox');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
   }
 }
 
